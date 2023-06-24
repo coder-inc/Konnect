c05fbb7d54ebd1124e862a2cde7871d9fd9ccbd6
Abhishek <checkmailabhi@gmail.com>
2023-06-24 06:24:58 +0530

    Started Frontend-Landing Page navbar

---
+++ Konnect/client/src/App.js
@@ -0,0 +1,24 @@
+import React from 'react' ;
+import NavBar from './components/NavBar' ;
+import './App.css' ;
+import {BrowserRouter,Route, Routes} from 'react-router-dom' ;
+import Home from './components/screens/Home' ;
+import Login from './components/screens/Login' ;
+import Profile from './components/screens/Profile' ;
+import Signup from './components/screens/Signup' ;
+
+function App() {
+  return (
+    <BrowserRouter>
+      <NavBar />
+      <Routes>
+          <Route exact path="/" element={<Home/>} />
+          <Route path="/signin" element={<Login/>} />
+          <Route path="/profile" element={<Profile/>} />
+          <Route path="/signup" element={<Signup/>} />
+        </Routes>
+    </BrowserRouter>
+  );
+}
+
+export default App;
