bc2b8200c0069354945f8767d777005099e18c09
Abhishek <checkmailabhi@gmail.com>
2023-06-27 16:44:44 +0530

    Uploading Files to cloudinary

--- Konnect/Server/Routes/post.js
+++ Konnect/Server/Routes/post.js
@@ -16,8 +16,8 @@ router.get('/allpost',(req,res)=>{
 })
 
 router.post('/createpost',requireLogin,(req,res)=>{
-    const {title,body} = req.body
-    if(!title || !body){
+    const {title,body,pic} = req.body
+    if(!title || !body || !pic){
         return res.status(422).json({error:"Please Add all the fields"}) ;
     }
     // console.log(req.user)
@@ -26,6 +26,7 @@ router.post('/createpost',requireLogin,(req,res)=>{
     const post = new Post({
         title,
         body,
+        pic, //picture to send to the cloudinary
         postedBy : req.user // who posted it
     })
     post.save().then(result=>{
--- Konnect/client/src/components/screens/CreatePost.js
+++ Konnect/client/src/components/screens/CreatePost.js
@@ -1,6 +1,55 @@
-import React from "react";
+import React,{useState} from "react";
+import M from "materialize-css" ;
+import {useNavigation} from 'react-router-dom'
 
 const CreatePost = ()=>{
+    const Navigate = useNavigation ;
+    const [title,setTitle] = useState("") ;
+    const [body,setBody] = useState("") ;
+    const [image,setImage] = useState("") ;
+    const [url,setUrl] = useState("") ;
+
+    const postDetails = ()=>{ //posting all the images at cloudinary
+        const data = new FormData() ; // if we want to upload a file then we need to do this
+        data.append("file",image) ;
+        data.append("upload_preset","konnect") ; // in cloudinary I gave my upload preset a name Konnect
+        data.append("cloud_name","konnectcnq") ; // cloud name is konnectcnq
+        fetch("https://api.cloudinary.com/v1_1/konnectcnq/image/upload",{
+            method:"post",
+            body:data
+        })// API base URl from cloudinary itself and appended /image/upload after link
+        .then(res=>res.json())
+        .then(data=>{
+            setUrl(data.url) ;
+        })
+        .catch(err=>{
+            console.log(err) ;
+        })
+        // separate network request to our server to post the data
+        fetch("/createpost",{  //Both node and react have different servers so inorder to send data to node we are sending our req from port 3000 to 5000 of mode sp added a proxy in package.json
+            method: "post" ,
+            headers: {
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify({
+                title: title,
+                body:body,
+                pic:url
+            })
+        }).then(res=>res.json())
+        .then(data=>{
+            console.log(data) ;
+            if(data.error){
+                M.toast({html: data.error,classes:"#c62828 red darken-3"}) ; //for creating pop-up message
+            }
+            else{
+                M.toast({html:"Created Post Successfully",classes:"#43a047 green darken-1"}) ;
+                Navigate('/')//Navigating the user to the home screen
+            }
+        }).catch(err=>{
+            console.log(err) ;
+        })
+    }
     return(
         <div className="card input-filed" style={{
             margin:"30px auto",
@@ -8,19 +57,29 @@ const CreatePost = ()=>{
             padding:"20px",
             textAlign:"center"
         }}>
-            <input type="text" placeholder="title"/>
-            <input type="text" placeholder="body"/>
+            <input 
+            type="text" 
+            placeholder="title"
+            value = {title}
+            onChange={(e)=>setTitle(e.target.value)}
+            />
+            <input type="text"
+            placeholder="body"
+            value = {body}
+            onChange={(e)=>setBody(e.target.value)}
+            />
             <div className="file-field input-field">
                 <div className="btn #64b5f6 blue darken-1">
                     <span>Upload image</span>
-                    <input type="file"/>
+                    <input type="file" onChange={(e)=>setImage(e.target.files[0])}/>
                 </div>
             <div className="file-path-wrapper">
                 <input className="file-path validate" type="text"/>
             </div>
             </div>
-            <button className="btn waves-effect waves-light #64b5f6 blue darken-1">
-                    Submit
+            <button className="btn waves-effect waves-light #64b5f6 blue darken-1"
+            onClick={()=>postDetails()}>
+                    Submit Post
             </button>
         </div>
     )
